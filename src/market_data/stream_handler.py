b51943f78cc019d0a71f477d0c38b2aaaf7a62ee
deanmasc <deanmascitti@gmail.com>
2026-08-18 01:00:32 +1000

    feat(stream): enhance BinanceSockethandler with additional stream constants and improve data retrieval logic

--- src/market_data/stream_handler.py
+++ src/market_data/stream_handler.py
@@ -1,18 +1,29 @@
 import asyncio, websockets, json
-from utils.constants import STREAM_URL
+from utils.constants import STREAM_URL, PARTIAL_DEPTH_STREAM_NAME, AGGREGATE_TRADES_STREAM_NAME
 
 class BinanceSockethandler:
     def __init__(self):
         self.ws = None
+        self.recv_data_mode = True
 
-    async def setup_connection(self):
+    async def setup_connection(self) -> None:
+        print("Setting up WebSocket connection.")
         self.ws = await websockets.connect(STREAM_URL)
+        print("WebSocket connection established.")
 
-    async def recv_data(self):
-        while (True):
+    async def recv_data(self) -> None:
+        print("Commencing stream data retrieval")
+        while (self.recv_data_mode):
             if self.ws is not None:
-                msg = await self.ws.recv()
-                print(msg)
+                try:
+                    msg = await self.ws.recv()
+                    print(f"Message recieved: {msg}")
+                finally:
+                    self.close_connection()
+        print("Stopped recieveing data")
 
-    async def close_connection(self):
+    async def close_connection(self) -> None:
         await self.ws.close()
+
+    def set_recv_data_mode(self, status: bool) -> None:
+        self.recv_data_mode = status
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,1 +1,3 @@
 STREAM_URL = "wss://stream.binance.com:9443/stream?streams=btcusdt@depth20@100ms/btcusdt@aggTrade"
+PARTIAL_DEPTH_STREAM_NAME = "btcusdt@depth20@100ms"
+AGGREGATE_TRADES_STREAM_NAME = "btcusdt@aggTrade"
