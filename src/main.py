ccee75f9c1b7376609ba6d0cacfe6af24144531b
deanmasc <deanmascitti@gmail.com>
2026-08-26 01:07:55 +1000

    feat: Fixing main.py so that it correctly creates an event loop and abides by async functions.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
+import asyncio
 from market_data.stream_handler import BinanceSockethandler
 
-def main():
+async def main():
     """
     Main entry function, we need to:
     - Establish connections with Binance Streams
     - Commence loop to run the loop of updating the dashboard in real time
     """
     connection_handler = BinanceSockethandler()
-    connection_handler.setup_connection() # creates websocket
-    connection_handler.recv_data() # loops on recieveing data
+    await connection_handler.setup_connection() # creates websocket
+    await connection_handler.recv_data() # loops on recieveing data
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
